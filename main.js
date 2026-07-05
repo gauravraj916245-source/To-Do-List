76b2b8fbc1c5b245141fc07e9361e8a07341d167
gaurav raj <gauravraj916245@gmail.com>
2026-07-05 19:43:56 +0530

    To Do List Project

---
+++ main.js
@@ -0,0 +1,30 @@
+const A =document.getElementById('task-area');
+const B =document.getElementById('single-task');
+const C =document.getElementById('tasks');
+
+function addTask(event){
+    event.preventDefault();
+    if(B.value==='')
+        return;
+    const task=createTask(B.value);
+    C.appendChild(task);
+    B.value='';
+}
+// create a new task
+function createTask(taskName){
+    const task=document.createElement('li');
+    task.classList.add('task');
+    task.innerHTML=`
+    <input type="checkbox">
+    <label>${taskName}</label>
+    <span class="delete">&times;</span>
+  `;
+  const deleteButton=task.querySelector('.delete');
+  deleteButton.addEventListener('click',deleteTask);
+  return task;
+
+}
+function deleteTask(event){
+    event.target.parentElement.remove();
+}
+A.addEventListener('submit',addTask);
